688e171c5d5a34b7633e95c4b17255b9b03a7ce1
mohsensami <mohsensami18727@gmail.com>
2024-09-21 19:06:06 +0330

    add custom Headerfor blog page

---
+++ src/app/blog/layout.tsx
@@ -0,0 +1,18 @@
+export default function ProductLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div className="container">
+            <div className="grid grid-cols-4">
+                <div className="col-span-3">{children}</div>
+                <div className="col-span-1">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Est sit eos modi nisi veritatis, ducimus,
+                    animi odio quod adipisci enim eius cumque ullam ratione aspernatur, eaque excepturi obcaecati non
+                    repellat.
+                </div>
+            </div>
+        </div>
+    );
+}
